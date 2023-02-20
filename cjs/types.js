f57b66147e138d9b3f16f892c37b57c563bdc047
Patrice THIMOTHEE <thimpat@gmail.com>
2023-02-20 22:54:20 +0000

    REFACTOR: Define types for jsdoc

---
+++ cjs/types.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef INVOKER_TYPE
+ * @property {string} funcName Function name of exported C function
+ * @property {string} cFunctionPrototype Prototype for exported C function
+ * @property {function} nodeFunctionInvoker Internal NodeJs function invokeFunctionFromTable
+ *
+ */
+
+/**
+ * @typedef FilePath
+ * @description File path
+ * @type string
+ */
+
+/**
+ * @typedef Folder
+ * @description Folder path
+ * @type string
+ */
